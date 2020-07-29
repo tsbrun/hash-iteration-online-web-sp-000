b762bf853133610eb037ec5e26c5134538edcadb
Anya Brun <tsbrun@icloud.com>
2020-07-29 16:24:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,3 +10,9 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{key}! You are now #{value} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |key, value|
+    puts "Happy Birthday #{key}! You are now #{value} years old!" unless value > 12
+  end
+end
