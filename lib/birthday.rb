ff2f56fe6be4d54f15ace4bced89b5df31ea5da8
Anya Brun <tsbrun@icloud.com>
2020-07-29 16:23:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,5 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  puts "Happy Birthday #{key}! You are now #{value} years old!"
+  birthday_kids.each do |key, value|
+    puts "Happy Birthday #{key}! You are now #{value} years old!"
+  end
 end
